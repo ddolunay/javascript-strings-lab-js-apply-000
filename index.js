a9182f8435ff92a8a06f31b62b703418ea89b27b
Ilkay Demir  Dolunay <demirdolunay@gmail.com>
2019-01-20 18:01:33 +0000

    Automatically backed up by Learn

--- index.js
+++
@@ -1,9 +0,0 @@
-var greeting = "Hello, everybody!";
-
-var specialGuest = "Neil deGrasse Tyson"
-
-var greetSpecialGuest = "Hello," + specialGuest + "!";
-
-var topic = "let\`s talk about space.";
-
-var conversation = `${let\`s talk about space.}`;\
