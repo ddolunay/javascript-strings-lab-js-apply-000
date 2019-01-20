242e39ca5299224a986de0c35cb370f0a9b67088
Ilkay Demir  Dolunay <demirdolunay@gmail.com>
2019-01-20 17:54:26 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,12 @@
+
+ it('defines `greeting`', function() {
+  assert.equal(greeting, "Hello, everybody!")
+})
+
+ it('concatenates strings to greet a special guest in `greetSpecialGuest`', function() {
+  assert.equal(greetSpecialGuest, "Hello, " + specialGuest + "!")
+})
+
+ it('interpolates a string in `conversation`', function() {
+  assert.equal(conversation, `Let's talk about ${topic}.`)
+})
